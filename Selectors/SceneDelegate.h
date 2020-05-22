74dec38c0cf3b1fb2526351335763149a9d6a120
devalmon <phlood@yandex.ru>
2020-05-22 08:38:35 +0300

    Initial Commit

---
+++ Selectors/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Selectors
+//
+//  Created by Alexey Baryshnikov on 22.05.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Selectors/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Selectors
+//
+//  Created by Alexey Baryshnikov on 22.05.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
